a78953fd574fa8656c3003f5002076c638331152
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-23 11:27:37 +0545

    add: icon looping and image.d.ts file

---
+++ src/constants/Icon.tsx
@@ -0,0 +1,38 @@
+export const web_technology_icon = [
+    { html: require("../assets/img/technology/html-5.png") },
+    { css: require("../assets/img/technology/css.png") },
+    { twail: require("../assets/img/technology/twail_css.jpg") },
+    { bootstrap: require("../assets/img/technology/bootstrap.png") },
+    { github: require("../assets/img/technology/github.png") },
+    { gitlab: require("../assets/img/technology/gitlab.png") },
+    { js: require("../assets/img/technology/js.png") },
+    { typescript: require("../assets/img/technology/typescript.png") },
+    { react: require("../assets/img/technology/react.png") },
+    { nodejs: require("../assets/img/technology/nodejs.png") },
+    { mysql: require("../assets/img/technology/mysql.png") },
+    { mongo: require("../assets/img/technology/icons8-mongodb-480.png") },
+    { postgre: require("../assets/img/technology/postgre.png") },
+    { typeorm: require("../assets/img/technology/typeorm.png") },
+    { npm: require("../assets/img/technology/npm.png") },
+    { docker: require("../assets/img/technology/docker.png") },
+    { kubernets: require("../assets/img/technology/kubernets.png") },
+    { aws: require("../assets/img/technology/aws.png") },
+    { vscode: require("../assets/img/technology/vscode.png") },
+    { wordpress: require("../assets/img/technology/wordpress.png") },
+    { firebase: require("../assets/img/technology/firebase.png") },
+    { figma: require("../assets/img/technology/figma.png") },
+    { illustrator: require("../assets/img/technology/illustrator.png") },
+    { indesign: require("../assets/img/technology/indesign.png") },
+    { photoshop: require("../assets/img/technology/photoshop.png") },
+    { c: require("../assets/img/technology/c.png") },
+    { cPlus: require("../assets/img/technology/c++.png") },
+    { go: require("../assets/img/technology/go.png") },
+    { java: require("../assets/img/technology/Java.jpeg") },
+    { laravel: require("../assets/img/technology/laravel.jpeg") },
+  ];
+  
+  export const mobile_technology_icon = [
+    { dart: require("../assets/img/technology/dart.png") },
+    { flutter: require("../assets/img/technology/flutter.png") },
+  ];
+  
